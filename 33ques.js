2ed78592f90f7663da15d24a481f5c1011e59800
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-19 14:50:43 +0500

    Add files via upload

---
+++ 33ques.js
@@ -0,0 +1,6 @@
+var favoritePizzas = ["Pepperoni", "Margherita", "Supreme"];
+console.log("My favorite pizzas are:");
+favoritePizzas.forEach(function (pizza) {
+    console.log("I really like ".concat(pizza));
+});
+console.log("I love pizza");
---
+++ 33ques.ts
@@ -0,0 +1,7 @@
+const favoritePizzas: string[] = ["Pepperoni", "Margherita", "Supreme"];
+
+console.log("My favorite pizzas are:");
+favoritePizzas.forEach(pizza => {
+    console.log(`I really like ${pizza}`:);
+});
+console.log("I love pizza")
