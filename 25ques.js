72be7d0f6654d4a19da9fbe0862b2db19fdc4ea4
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-19 11:49:30 +0500

    Add files via upload

---
+++ 25ques.js
@@ -0,0 +1,15 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+// version2 
+var alien_colors = 'red'; // Change 'red' to 'yellow' for a different outcome
+if (alien_colors === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
---
+++ 25ques.ts
@@ -0,0 +1,17 @@
+const alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+
+
+// version2 
+const alien_colors: string = 'red'; // Change 'red' to 'yellow' for a different outcome
+
+if (alien_colors === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
