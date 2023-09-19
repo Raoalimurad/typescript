f5b5db4c6915748075f0d9828656362f01bddecf
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-19 09:27:45 +0500

    Add files via upload

---
+++ 20ques.js
@@ -0,0 +1,16 @@
+var countries = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "Germany",
+    "France",
+    "Japan",
+    "Australia",
+    "Brazil",
+    "India",
+    "China"
+];
+console.log("the list of country ");
+countries.forEach(function (country) {
+    console.log(country);
+});
---
+++ 20ques.ts
@@ -0,0 +1,16 @@
+const countries: string[] = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "Germany",
+    "France",
+    "Japan",
+    "Australia",
+    "Brazil",
+    "India",
+    "China"
+];
+console.log("the list of country :");
+countries.forEach(country => {
+    console.log(country);
+});
