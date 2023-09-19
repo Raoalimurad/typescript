9f7160dbb34cfd5ddbd4bee3ce1881ceeea5fab1
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-19 11:34:13 +0500

    Add files via upload

---
+++ 24ques.js
@@ -0,0 +1,5 @@
+// version one statment is true 
+var aligenColor = "green";
+if (aligenColor = "green") {
+    console.log("Congratulations! You just earned 5 points.");
+}
---
+++ 24ques.ts
@@ -0,0 +1,15 @@
+
+// version one statment is true 
+let aligenColor="green";
+if(aligenColor="green"){
+    console.log("Congratulations! You just earned 5 points.");
+}
+
+
+
+
+// version 2 statment is wrong 
+let aligenColors="red";
+if(aligenColors="green"){
+    console.log("Congratulations! You just earned 5 points.");
+}/////no output find beacuse condition is wrong
