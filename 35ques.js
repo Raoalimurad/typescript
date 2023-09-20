6b40122a2039cb4f8086ccdcfc10bd3eabfc9999
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-20 09:12:48 +0500

    Add files via upload

---
+++ 35ques.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("my shirt size is ".concat(size, " and love to wear ").concat(message));
+}
+make_shirt("medium", "t-shirts");
---
+++ 35ques.ts
@@ -0,0 +1,4 @@
+function make_shirt(size:string,message:string):void{
+     console.log(`my shirt size is ${size} and love to wear ${message}`);
+}
+make_shirt("medium","t-shirts");
